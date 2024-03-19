ff7f6f55dbcd30e09d5f2124e9be32fe2c9e2530
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-03-19 12:55:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var message1 = "hello world";
+console.log(message1);
+var student = "Rehana";
+console.log("".concat(student, " is saing Hello to the programming world."));
---
+++ main.ts
@@ -0,0 +1,11 @@
+let message1 :string = "hello world";
+
+console.log(message1);
+
+
+
+
+
+
+// let student : string = "Rehana";
+// console.log(`${student} is saying Hello to the programming world.`)
